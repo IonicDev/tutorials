bf424cf8d60632b116debd20194906d4a21bb33f
Rick Ehrhart <rick@ionicsecurity.com>
2018-08-10 10:06:54 -0700

    Fixed spacing and quoting

--- javascript/README.md
+++ javascript/README.md
@@ -4,3 +4,5 @@ To build and run an Ionic JavaScript SDK Sample, navigate into the directory of 
 sample task and follow the steps below.  The Ionic JavaScript SDK is used in all the tutorials.
 Go to [Ionic Developer SDK Setup](https://dev.ionic.com/tutorials/getting-started/sdk-setup) 
 then click on the JavaScript icon and the desired browser icon.
+
+_Needs more verbiage_
--- javascript/sdk-basics/profiles/profiles.js
+++ javascript/sdk-basics/profiles/profiles.js
@@ -5,81 +5,82 @@
  */
 
 const appData = {
-    appId: 'ionic-js-samples',
-    userId: 'developer',
-    userAuth: 'password123',
-    enrollmentUrl: 'https://dev-enrollment.ionic.com/keyspace/HVzG/register'
+  appId: 'ionic-js-samples',
+  userId: 'developer',
+  userAuth: 'password123',
+  enrollmentUrl: 'https://dev-enrollment.ionic.com/keyspace/HVzG/register'
 }
 
 const main = async () => {
-    
-    // initialize agent
-    const agent = new window.IonicSdk.ISAgent('https://dev-api.ionic.com/jssdk/latest/');
-    await agent.loadUser(appData).catch((error) => {
-        console.log(`Error loading profile: ${error}`)
-    })
+  
+  // initialize agent
+  const agent = new window.IonicSdk.ISAgent('https://dev-api.ionic.com/jssdk/latest/')
+  await agent.loadUser(appData).catch((error) => {
+    console.log('Error loading profile: ', error)
+  })
 
-    // Get all the profiles.
-    const response = await agent.queryProfiles(appData).catch((error) => {
-        console.log(`Query profiles error: ${error}`)
-    })
-    const profiles = response.profiles 
+  // Get all the profiles.
+  const response = await agent.queryProfiles(appData).catch((error) => {
+    console.log('Query profiles error: ', error)
+  })
+  const profiles = response.profiles 
 
-    // Verify there is at least one profile.
-    if (profiles.length == 0) {
-        console.log(`No profiles found in specified profile persistor`);
-        return
+  // Verify there is at least one profile.
+  if (profiles.length == 0) {
+    console.log('No profiles found in specified profile persistor')
+    return
+  }
+      
+  // List all available profiles.
+  var index, len, active_device_id
+  console.log('ALL PROFILES:')
+  for (index = 0, len = profiles.length; index < len; ++index) {
+    console.log('---')
+    console.log('ID       : ', profiles[index].deviceId)
+    console.log('Created  : ', profiles[index].created)
+    console.log('Keyspace : ', profiles[index].keyspace)
+    console.log('ApiUrl   : ', profiles[index].server)
+
+    // Save active (passive) profile device ID for later.
+    if (profiles[index].active) {
+      active_device_id = profiles[index].deviceId
     }
-        
-    // List all available profiles.
-    var index, len, active_device_id
-    console.log( `ALL PROFILES:`);
-    for (index = 0, len = profiles.length; index < len; ++index) {
-        console.log(`---`)
-        console.log(`ID       : ${profiles[index].deviceId}`)
-        console.log(`Created  : ${profiles[index].created}`);
-        console.log(`Keyspace : ${profiles[index].keyspace}`)
-        console.log(`ApiUrl   : ${profiles[index].server}`)
+  }
 
-        // Save active (passive) profile device ID for later.
-        if (profiles[index].active) {
-            active_device_id = profiles[index].deviceId
-        }
+  // If the number of profiles is equal to one, then there is nothing to set.
+  if (profiles.length == 1) {
+    console.log('Only one profile, nothing to change')
+    return
+  }
+      
+  // Display active profile.
+  console.log('\nACTIVE PROFILE: ', active_device_id)
+
+  // Change the active profile.
+  const new_profile_id = 'HVzG.3.cf55cc46-bd06-4d00-9202-eb667f03eea4'
+  console.log('\nSETTING NEW ACTIVE PROFILE: ', new_profile_id)
+
+  // Define the profile to make active.
+  const profile_to_set =
+    { appId: appData.appId,
+      userId: appData.userId,
+      userAuth: appData.userAuth,
+      deviceId: new_profile_id
     }
+  
+  // Set the active profile.
+  const set_active_profile_resp = await agent.setActiveProfile(profile_to_set).catch((error) => {
+    console.log('Set active profile error: ', error)
+  })
 
-    // If the number of profiles is equal to one, then there is nothing to set.
-    if (profiles.length == 1) {
-        console.log(`Only one profile, nothing to change`)
-        return
+  // Loop through the list of profiles looking for active profile,
+  // and output information about the active profile.
+  const updated_profiles = set_active_profile_resp.profiles 
+  for (index = 0, len = updated_profiles.length; index < len; ++index) {
+    if (updated_profiles[index].active) {
+      console.log('\nNEW ACTIVE PROFILE: ', updated_profiles[index].deviceId)
     }
-        
-    // Display active profile.
-    console.log(`\nACTIVE PROFILE: ${active_device_id}`)
-
-    // Change the active profile.
-    const new_profile_id = 'HVzG.3.cf55cc46-bd06-4d00-9202-eb667f03eea4'
-    console.log(`\nSETTING NEW ACTIVE PROFILE: $new_profile_id`)
-
-    // Define the profile to make active.
-    const profile_to_set =
-        { appId: appData.appId,
-          userId: appData.userId,
-          userAuth: appData.userAuth,
-          deviceId: new_profile_id
-        }
-    
-    const set_active_profile_resp = await agent.setActiveProfile(profile_to_set).catch((error) => {
-        console.log(`Set active profile error: ${error}`)
-    })
-
-    // Loop through the list of profiles looking for active profile,
-    // and output information about the active profile.
-    const updated_profiles = set_active_profile_resp.profiles 
-    for (index = 0, len = updated_profiles.length; index < len; ++index) {
-        if (updated_profiles[index].active) {
-            console.log(`\nNEW ACTIVE PROFILE: ${updated_profiles[index].deviceId}`);
-        }
-    }
+  }
 }
 
-main();
+main()
